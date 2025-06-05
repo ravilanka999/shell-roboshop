07602b120bf297c58082cb6cd659b1f9a828a836
ravilanka999 <ravi050892@gmail.com>
2025-06-05 19:40:56 +1000

    Shell-Roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -77,6 +77,15 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
+# STATUS=$(mongosh --host mongodb.sindhu.agency --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+# if [ $STATUS -lt 0 ]
+# then
+#     mongosh --host mongodb.sindhu.agency </app/db/master-data.js &>>$LOG_FILE
+#     VALIDATE $? "Loading data into MongoDB"
+# else
+#     echo -e "Data is already loaded ... $Y SKIPPING $N"
+# fi
+
 STATUS=$(mongosh --host mongodb.sindhu.agency --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
@@ -85,4 +94,3 @@ then
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
-
